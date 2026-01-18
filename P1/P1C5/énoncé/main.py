19a774202157ecc6a294fcd26d78dd9d75920f11
Jordan <jordan.valente@hotmail.fr>
2026-01-18 18:09:45 +0100

    third exercice

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,18 @@
 # Écrivez votre code ici !
+nom = "Jordan"
+age = 37
+taille = 1.75
+est_etudiant = True
+
+print(f"Nom : {nom}")
+print(f"Age : {age}")
+print(f"Taille : {taille}")
+print(f"Étudiant : {est_etudiant}")
+
+print(f"Je m'appelle {nom}, j'ai {age} ans et je mesure {taille} ")
+
+
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type étudiant: {type(est_etudiant)}")
