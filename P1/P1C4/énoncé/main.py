82db825403ee28b4087d2959f38e349ab4f73eef
Jordan <jordan.valente@hotmail.fr>
2026-01-18 17:53:04 +0100

    exerciceP1C4

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+
+
+nom = "Jordan"
+age =36
+print(f"Bonjour, je m'appelle {nom}, et j'ai {age} ans.")
+
+age = 25
+
+print(f"Bonjour, je m'appelle {nom}, et j'ai {age} ans.")
